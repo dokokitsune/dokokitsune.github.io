3ddae1102b8106159bb756afd58abf09d3b18cc7
wjw1998 <stakin101@gmail.com>
2023-04-26 13:09:17 -0700

    Entered info now clears on submission

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-function clearText(){
-  document.getElementsByClassName("contactFormText").value = "";
+function clearAllText(){
+  document.getElementById("nameText").value = "";
+  document.getElementById("emailText").value = "";
+  document.getElementById("numberText").value = "";
+  document.getElementById("messageText").value = "";
 }
